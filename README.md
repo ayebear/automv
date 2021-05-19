6f0a68eedc207793022e66a0a36c2ffd7762dd4b
Eric Hebert <ayebear@gmail.com>
2021-05-19 14:01:55 -0400

    Rewrote logic to use native node.js fs libraries and cross platform packages Removed "dot" from temp filename for better cross-platform compatibility Should work on Windows now, but it still has only been tested on Linux

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # automv
 
-Simple daemon script to move files matching a glob pattern to an output directory. The moves are done atomically by using a .mvtmp file first. This will continue moving new files automatically, as long as it is running.
-
-## Requirements
-
-Must have the `mv` command available. Only tested working on a Linux system.
+Simple daemon script to move files matching a glob pattern to a list of output directories. The moves are done atomically by using a .mvtmp file first. This will continue moving new files automatically, as long as it is running. The output directories will be checked for enough free space to move the input files to, and round robin them as they fill up. Uses built-in fs operations, to be compatible with multiple platforms.
 
 ## Setup and run example
 
 ```
 yarn
-node index.js "tmp/*.plot" "/mnt/networkdrive/plots"
+node index.js "tmp/*.plot" "/mnt/networkdrive/plots" "/mnt/networkdrive/plots2" "/mnt/networkdrive/plots3"
 ```
--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 import path from 'path'
-import spawnAsync from '@expo/spawn-async'
+import fs from 'fs'
 import chokidar from 'chokidar'
 import PQueue from 'p-queue'
+import checkDiskSpace from 'check-disk-space'
+const { stat, copyFile, unlink, rename } = fs.promises
 
-async function move(queue, src, outDir, dry) {
-	console.log(`Queued move: ${src} to ${outDir}`)
-	return queue.add(async () => {
-		try {
-			const srcName = path.basename(src)
-			const tempName = `.${srcName}.mvtmp`
-			const tempPath = path.join(outDir, tempName)
-			const outPath = path.join(outDir, srcName)
-			console.log(`Moving (1/2): ${src} to ${tempPath}`)
-			if (!dry) {
-				await spawnAsync('mv', [src, tempPath])
-			}
-			console.log(`Moving (2/2): ${tempName} to ${srcName}`)
-			if (!dry) {
-				await spawnAsync('mv', [tempPath, outPath])
-			}
-		} catch (e) {
-			console.error(e)
+// Set to true to skip actual moves
+// TODO: Use environment variable
+const DRY = false
+
+// Find output directory with enough free space for src file
+// Throws if all are full
+async function getFreeDir(src, outDirs) {
+	const { size } = await stat(src)
+	for (const dir of outDirs) {
+		// Check if file will fit in output directory
+		const { free } = await checkDiskSpace(dir)
+		if (size < free) {
+			return dir
 		}
-	})
+	}
+	throw new Error('All specified output directories are full.')
+}
+
+async function move(src, outDirs) {
+	try {
+		// Get paths/filenames
+		const outDir = await getFreeDir(src, outDirs)
+		const srcName = path.basename(src)
+		const tempName = `${srcName}.mvtmp`
+		const tempPath = path.join(outDir, tempName)
+		const outPath = path.join(outDir, srcName)
+		// Atomic move (copy to temp, rename temp to final, delete original)
+		console.log(`Moving: ${src} to ${outPath}`)
+		if (DRY) {
+			console.log(`Dry run, skipped. (${srcName})`)
+			return
+		}
+		await copyFile(src, tempPath)
+		await rename(tempPath, outPath)
+		await unlink(src)
+		console.log(`Done! (${srcName})`)
+	} catch (e) {
+		console.error(e)
+	}
 }
 
 async function main() {
-	const [inGlob, outDir, dry] = process.argv.slice(2)
-	if (!inGlob || !outDir) {
-		console.error(`Usage: ${process.argv[1]} inGlob outDir [dry]`)
+	const [inGlob, ...outDirs] = process.argv.slice(2)
+	if (!inGlob || outDirs.length === 0) {
+		console.error(`Usage: ${process.argv[1]} inGlob <...outDirs>`)
 		return
 	}
 
@@ -36,8 +57,9 @@ async function main() {
 	const queue = new PQueue({ concurrency: 1 })
 
 	// Watch for new files to move
-	chokidar.watch(inGlob).on('add', path => {
-		move(queue, path, outDir, dry)
+	chokidar.watch(inGlob).on('add', src => {
+		console.log(`Queued move: ${src}`)
+		queue.add(() => move(src, outDirs))
 	})
 }
 
